44ebd16d89038bb5a423dad458cff5525274fc4d
aonych <aleksandrao995@gmail.com>
2017-01-22 20:55:33 +0100

    CRUD movie + character

---
+++ src/main/java/tjee/onych/service/CharacterService.java
@@ -0,0 +1,51 @@
+package tjee.onych.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import manager.CharacterManager;
+import manager.MovieManager;
+import tjee.onych.domain.Character;
+
+@Stateless
+public class CharacterService {
+	
+	@EJB
+	MovieManager movieManager;
+	@EJB
+	CharacterManager characterManager;
+	
+	public void add(Character character) {
+		if (!characterManager.isExistCharacter(character.getName())) {
+			characterManager.add(character);
+		}
+	}
+	
+	public List<Character> getByMovie(int id) {
+		return characterManager.getByMovie(id);
+	}
+
+	public void update(Character character) {
+		characterManager.update(character);
+	}
+
+	public void delete(int id) {
+		characterManager.delete(id);
+
+	}
+
+	public List<Character> getAll() {
+		return characterManager.getAll();
+	}
+
+	public Character getById(int id) {
+		return characterManager.getById(id);
+	}
+
+	public Character getByName(String name) {
+		return characterManager.getByName(name);
+	}
+	
+}
---
+++ src/main/java/tjee/onych/service/MovieService.java
@@ -0,0 +1,46 @@
+package tjee.onych.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import manager.MovieManager;
+import tjee.onych.domain.Movie;
+
+@Stateless
+public class MovieService {
+	
+	@EJB
+	MovieManager movieManager;
+	
+	public void add(Movie movie) {
+		if(!movieManager.isExistMovie(movie.getTitle()))
+			movieManager.add(movie);		
+	}
+
+	
+	public void update(Movie movie) {
+		movieManager.update(movie);	
+	}
+	
+	public void delete(int id) {
+		movieManager.delete(id);
+		
+	}
+
+	public List<Movie> getAll() {
+		return movieManager.getAll();
+	}
+
+	
+	public Movie getById(int id) {
+		return movieManager.getById(id);
+	}
+	
+	public Movie getByCountry(String country) {	
+		return movieManager.getByCountry(country);
+		
+	}
+	
+}
